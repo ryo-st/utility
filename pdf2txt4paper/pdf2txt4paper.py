6724aa1a83c9cb85ea1ec009b56981f9e7f20ef3
ryo-st <sato.ryo.so0@is.naist.jp>
2019-03-01 16:40:10 +0900

    Update parsing condition

--- pdf2txt4paper/pdf2txt4paper.py
+++ pdf2txt4paper/pdf2txt4paper.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-topicNumbers = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
+topicNumbers = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X','A','B','C','D','E','F','G','Fig','L','T','M']
 
 import sys
 lines = sys.stdin.readlines()
@@ -11,10 +11,23 @@ for line in lines:
         newline = newline + line
 
 newline=newline.replace('- ','')
+newline=newline.replace('   ',' ')
+newline=newline.replace('  ',' ')
 newline=newline.replace(".",".\n")
 newline=newline.replace("\n ","\n")
 
+newline=newline.replace("e.\ng.\n","e.g.")
+newline=newline.replace("i.\ne.\n","i.e.")
+newline=newline.replace("et al.\n","et al.")
+newline=newline.replace("Eq.\n","Eq.")
+
+#cnumber:Chapter number(1.1,1.2,...)
+cnumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
+for number in cnumber:
+    for number2 in cnumber:
+        newline=newline.replace(number+'.\n'+number2,number+'.'+number2)
+
 for topicNumber in topicNumbers:
     newline=newline.replace(topicNumber+'.\n',topicNumber+'.')
-    
+
 print (newline) 
